4a1d3be5d8f5cc941c02b0ca487bada6863a9ae9
SUDIPP22 <panja370@gmail.com>
2021-08-18 00:12:47 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/FindMaxValue.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To find maximum value using Generics
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-17
+ */
+public class FindMaxValue {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Program To Find Maximum Value Using Generics");
+    }
+}
