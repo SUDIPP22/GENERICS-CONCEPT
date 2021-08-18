688c1436de785334423ee57d67f6532056bc7a06
SUDIPP22 <panja370@gmail.com>
2021-08-18 15:01:01 +0530

    Refactored : Extending the program and printing the maximums using std out

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -17,22 +17,23 @@ public class FindMaxValue<E extends Comparable<E>> {
 
     /**
      * This method is used to compare the three  type of values and
-     * get the maximum values by using Generics
-     * @return maximum of three values
+     * get the maximum values by using Generics and
+     * printing the output
      */
-    public static <E extends Comparable<E>> E testMaximum(E[] myArray) {
+    public static <E extends Comparable<E>> void testMaximum(E[] myArray) {
         Arrays.sort(myArray);
         int range = myArray.length;
-        return myArray[range - 1];
+        E max = myArray[range - 1];
+        System.out.printf("Maximum Value is : %s\n", max);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome To The Program To Find Maximum Value Using Generics");
         Integer[] x1 = {10, 20, 30, 40, 50, 60};
-        System.out.println("The Maximum Integer Value is : " + testMaximum(x1));
+        testMaximum(x1);
         Float[] f1 = {50.02f, 6.3f, 20.36f, 51.9f, 52.36f, 52.89f};
-        System.out.println("The Maximum Float Value is : " + testMaximum(f1));
+        testMaximum(f1);
         String[] s1 = {"Mango", "Orange", "Pineapple", "Cherry", "Guava", "Water Melon"};
-        System.out.println("The Maximum String Value is : " + testMaximum(s1));
+        testMaximum(s1);
     }
 }
